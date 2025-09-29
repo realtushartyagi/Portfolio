30cd8823291e7af4d635b6031537b67824aff8aa
Tushar Tyagi <tushartyagi7575@gmail.com>
2025-09-29 15:02:24 +0530

    delete

--- public/index.ts
+++
@@ -1,6 +0,0 @@
-import backend from './backend.png';
-import creator from './creator.png';
-import web from './web.png';
-import mobile from './mobile.png';
-
-export {backend, creator, web, mobile}
