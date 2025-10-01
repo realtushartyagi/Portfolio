ad047397d9601b2ef2e3954dbfb154aa97d047d1
Tushar Tyagi <tushartyagi7575@gmail.com>
2025-10-02 03:09:02 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -5,3 +5,14 @@ type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 type PartiallyMutable<T, K extends keyof T> =
   { readonly [P in Exclude<keyof T, K>]: T[P] } &
   { -readonly [P in Extract<keyof T, K>]: T[P] };
+
+
+// Build route strings like "/users/123/posts/456"
+type ID = string | number;
+type Route<Path extends string, IdName extends string = 'id'> =
+  `/${Path}/${IdName}` | `/${Path}`;
+
+// Deep readonly using recursive mapped types (simple)
+type DeepReadonly<T> = T extends Function ? T : {
+  readonly [K in keyof T]: DeepReadonly<T[K]>;
+};
